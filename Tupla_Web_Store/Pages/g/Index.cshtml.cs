c05b88048585ceb266d0a7e1cc7ca56229fbc375
BlacZman <37856140+BlacZman@users.noreply.github.com>
2020-05-04 16:33:16 +0700

    Update _layout

--- Tupla_Web_Store/Pages/g/Index.cshtml.cs
+++ Tupla_Web_Store/Pages/g/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Tupla.Data.Core.GameData;
 
 namespace Tupla_Web_Store.Pages.g
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         private readonly IGame db;
